9dc539e2c59ad6cdb25546f015c59b40426c43cd
Amo Mensah <111157902+hyperactive-panda111@users.noreply.github.com>
2023-09-18 17:39:14 +0000

    Update 5 files. Commiting files for the Header section.

---
+++ app/games/[slug]/page.tsx
@@ -0,0 +1,7 @@
+import {NextPage} from 'next'
+
+const GameItem: NextPage = (props) => {
+  return <div>Games</div>;
+};
+
+export default GameItem;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,38 +1,39 @@
-import './globals.css'
-import { Montserrat, Poppins } from 'next/font/google'
-import { Metadata } from 'next'
+import "./globals.css";
+import { Montserrat, Poppins } from "next/font/google";
+import { Metadata } from "next";
+import Header from "/components/Header/Header";
 
 const poppins = Poppins({
   subsets: ["latin"],
   weight: ["400", "500", "700"],
   style: ["normal", "italic"],
   variable: "--font-poppins",
-})
+});
 
 const monserrat = Montserrat({
   subsets: ["latin"],
   weight: ["400", "500", "700"],
   style: ["normal", "italic"],
   variable: "--font-monserrat",
-})
+});
 
-export const metadata : Metadata = {
-  title: 'Ecommerce Nextjs',
-  description: 'Online Gaming Store',
-}
+export const metadata: Metadata = {
+  title: "Ecommerce Nextjs",
+  description: "Online Gaming Store",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className = {`${poppins.variable} ${monserrat.variable}`}>
-      <body >
-        <header>Header</header>
-        {children}
+    <html lang="en" className={`${poppins.variable} ${monserrat.variable}`}>
+      <body>
+        <Header />
+        <main className="bg-primary-gradient min-h-screen">{children}</main>
         <footer>Footer</footer>
       </body>
     </html>
-  )
+  );
 }
