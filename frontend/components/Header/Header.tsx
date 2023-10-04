3dda7e002f8eef08a8ed34ce9485c0729802dd17
Amo Mensah <111157902+hyperactive-panda111@users.noreply.github.com>
2023-10-04 03:00:20 +0000

    updating Header component to fix sign-up behaviour

--- frontend/components/Header/Header.tsx
+++ frontend/components/Header/Header.tsx
@@ -2,8 +2,8 @@
 
 import Link from "next/link";
 import headerClassNames from "./HeaderClassNames";
-import {AiOutlineShoppingCart} from 'react-icons/ai';
-import {FcGoogle} from 'react-icons/fc';
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import { FcGoogle } from "react-icons/fc";
 import { useAppDispatch } from "@/hooks/storeHook";
 import { toggleCart } from "@/redux/features/cartSlice";
 import useCartTotals from "@/hooks/useCartTotals";
@@ -11,87 +11,81 @@ import Signup from "../Signup/Signup";
 import { useState } from "react";
 
 const Header = () => {
-    const {
-        header, 
-        container,
-        li,
-        logoContainer,
-        link,
-        logo,
-        nav,
-        ul,
-        orders,
-        contactUs,
-        signupBtn,
-        signinBtn,
-        logoutBtn,
-        cart,
-    } = headerClassNames;
+  const {
+    header,
+    container,
+    li,
+    logoContainer,
+    link,
+    logo,
+    nav,
+    ul,
+    orders,
+    contactUs,
+    signupBtn,
+    signinBtn,
+    logoutBtn,
+    cart,
+  } = headerClassNames;
 
-    const {totalQuantity} = useCartTotals();
-    const [isSignUpFormOpen, setIsSignupFormOpen] = useState(false);
+  const { totalQuantity } = useCartTotals();
+  const [isSignUpFormOpen, setIsSignupFormOpen] = useState(false);
 
-    const dispatch = useAppDispatch();
+  const dispatch = useAppDispatch();
 
-    const toggleForm = () => {
-        setIsSignupFormOpen(!isSignUpFormOpen);
-    };
-    
-    return (
-        <>
-        <Signup isSignUpFormOpen
-         toggleForm={toggleForm}
-         />
-        <header className={header}>
-            <div className={container}>
-                <Link href='/' className={logoContainer}>
-                     <h1 className={logo}>Logo</h1>
+  const toggleForm = () => {
+    setIsSignupFormOpen(!isSignUpFormOpen);
+  };
+
+  return (
+    <>
+      <Signup 
+       isSignUpFormOpen={isSignUpFormOpen}
+       toggleForm={toggleForm} />
+      <header className={header}>
+        <div className={container}>
+          <Link href="/" className={logoContainer}>
+            <h1 className={logo}>Logo</h1>
+          </Link>
+
+          <nav className={nav}>
+            <ul className={ul}>
+              <li>
+                <button onClick={() => dispatch(toggleCart())} className={link}>
+                  <span>
+                    Cart
+                    <AiOutlineShoppingCart className="inline-block text-3xl" />
+                  </span>
+                  <div className={cart}>{totalQuantity}</div>
+                </button>
+              </li>
+
+              <li className="flex items-center justify-center h-7">
+                <Link href="/orders" className={orders}>
+                  Orders
                 </Link>
-
-                <nav className={nav}>
-                    <ul className={ul}>
-                        <li>
-                            <button onClick={() => dispatch(toggleCart())} className = {link}>
-                                <span>
-                                Cart
-                                <AiOutlineShoppingCart className="inline-block text-3xl" />
-                                  </span>
-                                  <div className={cart}>{totalQuantity}</div>
-                                </button>
-                        </li>
-
-                        <li className="flex items-center justify-center h-7">
-                            <Link href="/orders" className={orders}>
-                                Orders
-                            </Link>
-                            <button className={logoutBtn}>Logout</button>
-                            <button 
-                            onClick={toggleForm}
-                            className={signupBtn}>
-                                Sign Up
-                            </button>
-                            <button className={signinBtn}>
-                                Sign In
-                                <FcGoogle
-                                    style={{
-                                        fontSize: "25px",
-                                        cursor: "pointer",
-                                        marginLeft: "12px",
-                                    
-                                    }}
-                                    className={link}
-                                    />
-                                </button>
-                        </li>
-                    </ul>
-                      
-
-                </nav>
-                </div>
-                </header>
-        </>
-        
-    );
+                <button className={logoutBtn}>Logout</button>
+                <button onClick={toggleForm} className={signupBtn}>
+                  Sign Up
+                </button>
+                <button className={signinBtn}>
+                  Sign In
+                  <FcGoogle
+                    style={{
+                      fontSize: "25px",
+                      cursor: "pointer",
+                      marginLeft: "12px",
+                    }}
+                    className={link}
+                  />
+                </button>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+    </>
+  );
 };
 
-export default Header; 
+export default Header;
